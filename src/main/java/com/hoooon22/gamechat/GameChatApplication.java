1559166101e4ff88aee7d6ac14b9e56c91f69b32
Hoooon22 <momo990305@gmail.com>
2023-11-06 17:38:42 +0900

    SpringBoot Upload

---
+++ src/main/java/com/hoooon22/gamechat/GameChatApplication.java
@@ -0,0 +1,13 @@
+package com.hoooon22.gamechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoooon22/gamechat/GameChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoooon22.gamechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
